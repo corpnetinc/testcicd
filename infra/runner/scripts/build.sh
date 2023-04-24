dfc04655ef77848c126c9f2cf27e7bd20f5045b5
Iurii Egorov <Iurii_Egorov@epam.com>
2023-04-24 14:48:02 +0300

    Node in Docker

--- infra/runner/scripts/build.sh
+++ infra/runner/scripts/build.sh
@@ -2,7 +2,7 @@
 
 GH_RUNNER_VERSION_LABEL=$(curl -s -X GET 'https://api.github.com/repos/actions/runner/releases/latest' | jq -r '.tag_name')
 GH_RUNNER_VERSION=$(echo "${GH_RUNNER_VERSION_LABEL:1}")
-curl -o actions.tar.gz --location "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-x64-${GH_RUNNER_VERSION}.tar.gz"
+curl -o actions.tar.gz -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-x64-${GH_RUNNER_VERSION}.tar.gz"
 tar -zxf actions.tar.gz
 rm -f actions.tar.gz
 ./bin/installdependencies.sh
